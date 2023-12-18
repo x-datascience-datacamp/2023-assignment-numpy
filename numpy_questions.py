4667d6fdfac926edce682e297fe5a1616d691dc9
Louise Davy <davylouise@gmail.com>
2023-12-18 16:12:16 +0100

    fix : no more for loop

--- numpy_questions.py
+++ numpy_questions.py
@@ -65,10 +65,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    pi = 1.0
-    for i in range(1, n_terms + 1):
-        numerator = (2 * i) ** 2
-        denominator = (2 * i - 1) * (2 * i + 1)
-        pi *= numerator / denominator
-    pi *= 2
+    numerator = 4*(np.arange(1, n_terms + 1, dtype='float64'))**2
+    denominator = 4*np.arange(1, n_terms + 1, dtype='float64')**2 - 1
+    pi = 2 * np.prod(numerator / denominator)
     return pi
