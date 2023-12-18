edba1671a8588b47ed064b9cb086427e66d020f4
Ion VPS <ionpanteleiciuc@gmail.com>
2023-12-18 11:54:47 +0100

    First assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,11 +37,24 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+    if not isinstance(X, np.ndarray):
+      raise ValueError("Input is not a NumPy array")
+    
+    if X.ndim != 2:
+        raise ValueError("Input array shape is not 2D")
+
     i = 0
     j = 0
 
     # TODO
-
+    max = X[0][0]
+    for row_idx, row in enumerate(X):
+      for col_idx, col in enumerate(row):
+        if col > max: 
+          max = col
+          i = row_idx
+          j = col_idx
+          
     return i, j
 
 
@@ -64,4 +77,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    res = 2.
+    if n_terms == 0: return res
+    
+    for n in range(1, n_terms + 1):
+      res *= (4*n**2 / (4*n**2 - 1))
+    
+    return res
