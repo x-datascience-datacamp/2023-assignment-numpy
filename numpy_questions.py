b154a5cb33e4b18b4065deb822bb435d0c5cbb97
nathan.maligeay <nathan.maligeay@universite-paris-saclay.fr>
2023-12-18 15:07:45 +0100

    finished numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,8 +37,10 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("Input should be a 2D numpy array")
+
+    (i,j) = np.unravel_index(np.argmax(X), X.shape)   
 
     # TODO
 
@@ -64,4 +66,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    accu = 1
+    for n in range(1,n_terms+1):
+        accu *= ((4*n**2)/((4*n**2)-1))    
+    return accu*2
