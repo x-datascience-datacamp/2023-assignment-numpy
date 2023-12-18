00c0e02cffa709826cc46612a289a007c328cb42
Phuoc Ho <phuochoquang97@gmail.com>
2023-12-18 16:02:55 +0100

    complete max_index and wallis_product function

--- numpy_questions.py
+++ numpy_questions.py
@@ -40,7 +40,14 @@ def max_index(X):
     i = 0
     j = 0
 
-    # TODO
+    if type(X) is not np.ndarray:
+        raise ValueError("input is not a numpy array")
+    elif len(X.shape) != 2:
+        raise ValueError("the shape is not 2D")
+
+    idx = X.argmax()
+    i = idx // X.shape[1]
+    j = idx % X.shape[1]
 
     return i, j
 
@@ -64,4 +71,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 1.0
+    if n_terms == 0:
+        return pi * 2
+
+    for i in range(1, n_terms + 1):
+        pi *= 4 * i**2 / (4 * i**2 - 1)
+
+    return 2 * pi
