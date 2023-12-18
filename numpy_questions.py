e03bb863e3b5aefdcda7027acc8e742c0e0d9cba
Stéphane Dumas <stephane.dumas.pro@gmail.com>
2023-12-18 15:45:21 +0100

    Up bis

--- numpy_questions.py
+++ numpy_questions.py
@@ -72,8 +72,8 @@ def wallis_product(n_terms):
     w_p = 1
     if n_terms == 0:
         return w_p * 2
-
-    for n in range(1, n_terms + 1):
-        w_p *= (4*(n)**2) / (4*(n)**2-1)
+    x = np.arange(1, n_terms + 1)
+    x = 4 * x**2 / (4 * x**2 - 1)
+    w_p = np.prod(x)
 
     return w_p * 2
