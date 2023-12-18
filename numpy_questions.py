1585d913c23798eca7a95abd13f98ccfd22730a6
AugustinCreusillet <augustin.creusillet@gmail.com>
2023-12-18 15:42:18 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,6 +37,7 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+
     i = 0
     j = 0
 
@@ -44,16 +45,12 @@ def max_index(X):
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise ValueError("X should be a 2D numpy array")
-
     i, j = np.unravel_index(np.argmax(X), X.shape)
-
-    return i,j
-
+    return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
@@ -75,7 +72,7 @@ def wallis_product(n_terms):
 
     if n_terms == 0:
         return pi * 2
-    
+
     for i in range(1, n_terms + 1):
         pi *= (4 * i**2) / (4 * i**2 - 1)
 
