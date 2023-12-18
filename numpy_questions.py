b9fe4c0a8860fc7c29ff1815b95784143b852544
Louise Davy <davylouise@gmail.com>
2023-12-18 15:07:58 +0100

    feat : completed both functions

--- numpy_questions.py
+++ numpy_questions.py
@@ -29,7 +29,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,11 +37,12 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array.")
+    if X.ndim != 2:
+        raise ValueError("Input must be a 2D array.")
 
-    # TODO
-
+    i, j = np.unravel_index(np.argmax(X), X.shape)
     return i, j
 
 
@@ -64,4 +65,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 1.0
+    for i in range(1, n_terms + 1):
+        numerator = (2 * i) ** 2
+        denominator = (2 * i - 1) * (2 * i + 1)
+        pi *= numerator / denominator
+    pi *= 2
+    return pi
