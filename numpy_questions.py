f73b29ef179636dfb965edba7026619ae24e6d2a
Marius Ortega <ortegasaurelmarius@gmail.com>
2023-12-18 13:50:17 +0100

    assignment 1 complete

--- numpy_questions.py
+++ numpy_questions.py
@@ -17,7 +17,6 @@ errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
 
@@ -37,10 +36,10 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
-    # TODO
+    try:
+        i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    except:
+        raise ValueError("The input is not a numpy array or the shape is not 2D")
 
     return i, j
 
@@ -62,6 +61,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    
+    pi = 2.0
+    if n_terms != 0:
+        for i in range(1, n_terms+1):
+            pi = pi * (2. * i)/(2. * i - 1.) * (2. * i)/(2. * i + 1.) 
+    return pi
+
+wallis_product(100)
