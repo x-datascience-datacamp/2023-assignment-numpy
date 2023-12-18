fdb2f8528b0fff830b2767c0985bec976ecf281b
CodeurGarches <eric.patarin@yahoo.com>
2023-12-18 15:04:45 +0100

    UP my solution EricPatarin CodeurGarches

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,7 +39,10 @@ def max_index(X):
     """
     i = 0
     j = 0
-
+    if isinstance(X, np.ndarray) and X.ndim == 2:
+        i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    else:
+        raise ValueError
     # TODO
 
     return i, j
@@ -64,4 +67,15 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2.0
+
+    result = 1.0
+    for i in range(1, n_terms + 1):
+        term = 4.0 * i ** 2
+        result *= term / (term - 1)
+
+        # Multiply by 2 to get the final Wallis product
+    pi_approximation = 2 * result
+
+    return pi_approximation
