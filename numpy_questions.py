ef19507ffeabc7e44670e6159831542c92e9e692
Vivek <vivek.ashokan@universite-paris-saclay.fr>
2023-12-18 23:07:20 +0100

    questions2

--- numpy_questions.py
+++ numpy_questions.py
@@ -71,6 +71,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 1.0  # Si n_terms est 0, le produit est 1.
+
+    n = np.arange(1, n_terms + 1)
+    terms = (4 * n * n) / (4 * n * n - 1)
+    pi_approx = 2.0 * np.prod(terms)
+
+    return pi_approx
