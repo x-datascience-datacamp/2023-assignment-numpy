3b36d679f99af1405f33228f6e95cd4f9d5a34e3
michel-debug <724915929@qq.com>
2023-12-18 23:09:42 +0100

    main branch

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,10 +37,17 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
+    i = 123
     j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array.")
+
+    if X.ndim != 2:
+        raise ValueError("Input array must be 2D.")
+
+    # Find the index of the maximum value
+    i, j = np.unravel_index(np.argmax(X), X.shape)
 
     return i, j
 
@@ -64,4 +71,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi_approximation = np.prod(
+            [(4 * i * i) / (4 * i * i - 1) for i in range(1, n_terms)]
+        )
+    return pi_approximation
