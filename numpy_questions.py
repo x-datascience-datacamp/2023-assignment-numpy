7e39edd2a648a23772db37a5190cc65e57ad0485
Jules Dupont <jules.dupont@supagro.fr>
2023-12-18 15:30:58 +0100

    feat: propose solution for the assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,10 +37,13 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray):
+        raise ValueError("X is not a numpy array")
+    if X.ndim != 2:
+        raise ValueError("X is not 2D")
 
-    # TODO
+    index = np.argmax(X)
+    i, j = np.unravel_index(index, X.shape)
 
     return i, j
 
@@ -62,6 +65,7 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    iterations = np.arange(1, n_terms + 1)
+    numerator = 4 * np.power(iterations, 2)
+    denominator = 4 * np.power(iterations, 2) - 1
+    return 2 * np.prod(numerator / denominator)
