c6bcefea479d88e1de0617d7b6153040b45df6dc
CelineDev2011 <celinedeiva20@gmail.com>
2023-12-19 14:28:32 +0100

    Correct version -Assignment 1

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,11 +1,11 @@
-"""Assignment - using numpy and making a PR.#
+"""Assignment - using numpy and making a PR.
 
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
     * Use automated tools to validate the code (`pytest` and `flake8`)
     * Submit a Pull-Request on github to practice `git`.
 
-#The two functions below are skeleton functions. The docstrings explain what
+The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
 wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
@@ -29,7 +29,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and column index of the maximum.
+        The row and columnd index of the maximum.
 
     Raises
     ------
@@ -37,13 +37,11 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    # TODO
-    if not isinstance(X, np.ndarray):
-        raise ValueError("X is not a numpy array")
-    if not X.ndim == 2:
-        raise ValueError("X is not 2D")
-
-    return np.unravel_index(np.argmax(X, axis=None), X.shape)
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError('non relevant format')
+    else:
+        i, j = np.unravel_index(X.argmax(), X.shape)
+        return i, j
 
 
 def wallis_product(n_terms):
@@ -63,14 +61,10 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    product = 1
-    
-    for i in range(1, n_terms + 1):
-        product *= (4 * i ** 2) / (4 * i ** 2 - 1)
-        pi = 2 * product
-    return pi
-
-
-
-
-
+    if n_terms == 0:
+        return 2
+    else:
+        output = 1
+        for i in range(1, n_terms + 1):
+            output = output * (4.0 * (i ** 2)) / (4.0 * (i ** 2) - 1)
+        return 2 * output
