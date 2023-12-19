17d4e6ffe2a256572d19c51cb8ce73ffd56e47a9
lambi199 <lamichalop@gmail.com>
2023-12-19 14:10:25 +0100

    Version 5

--- numpy_questions.py
+++ numpy_questions.py
@@ -71,12 +71,10 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
-    if n_terms == 0:
-        return 1.0
-    else:
-        terms = np.arange(1, n_terms + 1)
-        numerator = 4 * terms**2
-        denominator = numerator - 1
-        return 2 * np.prod(numerator / denominator)
+    
+    terms = np.arange(1, n_terms + 1)
+    numerator = 4 * terms**2
+    denominator = numerator - 1
+    return 2 * np.prod(numerator / denominator)
 
 
