e48a9af4d890f07b23b36ea6e9eca342635d2653
HaythamBorchani <haytham.borchani@ensta-paris.fr>
2023-12-20 10:04:32 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,22 +1,4 @@
-"""Assignment - using numpy and making a PR.
-
-The goals of this assignment are:
-    * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
-
-The two functions below are skeleton functions. The docstrings explain what
-are the inputs, the outputs and the expected error. Fill the function to
-complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
-the repo. It should say that 2 tests ran with success.
-
-We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
-"""
 import numpy as np
-
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
@@ -29,7 +11,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,13 +19,11 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError("Input must be a 2D numpy array.")
 
-    # TODO
-
+    i, j = np.unravel_index(np.argmax(X), X.shape)
     return i, j
-
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -62,6 +42,16 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if not isinstance(n_terms, int) or n_terms < 0:
+        raise ValueError("Number of terms must be a non-negative integer.")
+
+    if n_terms == 0:
+        return 2.0
+
+    terms = np.arange(1, n_terms + 1).astype(np.float64)
+    print(terms)
+    wallis_array = (4.0 * terms ** 2) / ((4.0 * terms ** 2) - 1)
+    pi_approximation = 2.0 * np.prod(wallis_array)
+    print(n_terms)
+    print(pi_approximation)
+    return pi_approximation
