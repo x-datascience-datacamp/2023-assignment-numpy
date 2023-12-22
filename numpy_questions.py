f36ba4140a25417407b0844a709e3f65c4d94221
marinetognia <togniamarine@gmail.com>
2023-12-22 13:22:17 +0100

    update

--- numpy_questions.py
+++ numpy_questions.py
@@ -42,12 +42,10 @@ def max_index(X):
 
     if not isinstance(X, np.ndarray):
         raise ValueError('The input is not a numpy array')
-    
     if X.ndim != 2:
         raise ValueError('The shape is not 2D')
 
     i, j = np.unravel_index(np.argmax(X), X.shape)
-    
     return i, j
 
 
@@ -73,12 +71,9 @@ def wallis_product(n_terms):
 
     if n_terms == 0:
         return 2.0
-    
     pi = 2.0
 
-    for n in range(1, n_terms + 1):
-        
+    for n in range(1, n_terms+1):
         wallis_product = (4.0 * n ** 2) / ((4.0 * n ** 2) - 1)
         pi *= wallis_product
-
     return pi
