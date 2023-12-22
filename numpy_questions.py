7f785f19b43448ac143150752a91a7858c9e4a2b
Guimont <math.guimont@gmail.com>
2023-12-22 18:59:59 +0100

    deuxieme_commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -12,26 +12,27 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
     ValueError
         If the input is not a numpy array or
         if the shape is not 2D.
-    """    
+    """
     if not isinstance(X, np.ndarray):
-            raise ValueError("L'imput doit être un array numpy")
-    
+        raise ValueError("Input must be a NumPy array")
+
     if X.ndim != 2:
-        raise ValueError("L'array doit être en 2D")
-    # On trouve l'index du max
+        raise ValueError("Array must be 2D")
+
+    # On trouve l'indice du maximum
     max_index = np.argmax(X)
 
-    # On convertit max_index en 
+    # On convertit max_coords en coordonnées
     max_coords = np.unravel_index(max_index, X.shape)
 
-    return max_coords 
+    return max_coords
 
 
 def wallis_product(n_terms):
@@ -51,9 +52,8 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    pi=1    
-    #Calcul du produit 
-    for i in range(1,n_terms+1):
-        
-        pi=pi*((2*i/((2*i)-1))) *((2*i/((2*i)+1)))    
-    return pi*2
+    pi = 1
+    # Calcul du produit
+    for i in range(1, n_terms + 1):
+        pi *= (2 * i / (2 * i - 1)) * (2 * i / (2 * i + 1))
+    return pi * 2
