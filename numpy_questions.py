023d4854527ae96468eb81ce551495300a67bfb5
Pierre Fihey <121501990+Pierrefi@users.noreply.github.com>
2023-12-22 11:06:47 +0100

    Up numpy.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -2,18 +2,18 @@
 
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
+    * Use automated tools to validate the code (pytest and flake8)
+    * Submit a Pull-Request on github to practice git.
 
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
+wrote. To run the tests, use pytest test_numpy_question.py at the root of
 the repo. It should say that 2 tests ran with success.
 
 We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
+This will be enforced with flake8. You can check that there is no flake8
+errors by calling flake8 at the root of the repo.
 """
 import numpy as np
 
@@ -41,7 +41,6 @@ def max_index(X):
         raise ValueError("Input must be a 2D NumPy array.")
 
     i, j = np.unravel_index(np.argmax(X), X.shape)
-
     return i, j
 
 
@@ -54,15 +53,14 @@ def wallis_product(n_terms):
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+        Number of steps in the Wallis product. Note that n_terms=0 will
+        consider the product to be 1.
 
     Returns
     -------
     pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
+        The approximation of order n_terms of pi using the Wallis product.
     """
     n = np.arange(1, n_terms + 1)
     product = np.prod((4 * n*2) / (4 * n*2 - 1))
-    
     return product * 2
